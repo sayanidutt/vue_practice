24b0d28362ca7c21009dabea3e881269721dd1ba
Sayani <sayani.dutta@codelogicx.com>
2021-08-04 22:31:44 +0530

    upto section 8 introducing props

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <section>
+    <header><h1>My Friends</h1></header>
+    <ul>
+      <friend-contact
+        name="Peter"
+        phone-number="8945677566"
+        email-address="peter@gmail.com">
+      </friend-contact>
+      <friend-contact
+        name="John"
+        phone-number="8941267894"
+        email-address="john@gmail.com">
+      </friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "std1",
+          name: "Peter",
+          phoneno: "8945677566",
+          email: "peter@gmail.com",
+        },
+        {
+          id: "std2",
+          name: "John",
+          phoneno: "89412456322",
+          email: "john@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,40 @@
+<template>
+
+<li>
+    <h2>{{ name }}</h2>
+    <!--<button @click="toggleDetails">{‌{ detailsAreVisible ? 'Hide' : 'Show' }}Details</button>-->
+    <button @click="toggleDetails">{{ !detailsAreVisible?'Show Details':'Hide Details' }}</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+        <li><strong>Email:</strong>{{ emailAddress }}</li>
+    </ul>
+</li>
+    
+</template>
+
+<script>
+export default {
+    props: [
+    'name',
+    'phoneNumber',
+    'emailAddress'
+  ],
+    data(){
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'std1',
+                name: 'Peter',
+                phoneno: '8945677566',
+                email: 'peter@gmail.com'
+            }
+        };
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+
+};
+</script>
