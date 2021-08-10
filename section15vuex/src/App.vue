93afbd9bafa46e3dbd8a07d7ddb2cbb10f7c5a76
Sayani <sayani.dutta@codelogicx.com>
2021-08-10 22:16:12 +0530

    upto section 15 till introducing mutations

---
+++ section15vuex/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import TheCounter from './components/TheCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+  },
+  methods: {
+    addOne(){
+      this.$store.commit('increment');
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ section15vuex/src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue';
+import { createStore } from 'vuex';
+
+import App from './App.vue';
+
+const store = createStore({
+    state() {
+        return {
+            counter: 0,
+        };
+    },
+    mutations: {
+        increment(state){
+            state.counter = state.counter + 2;
+        }
+    }
+});
+
+const app = createApp(App);
+
+app.use(store);
+
+app.mount('#app');
