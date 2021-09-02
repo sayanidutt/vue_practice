04cfc24bbe840224b4bbae2c1b6f768336fcb245
Sayani <sayani.dutta@codelogicx.com>
2021-09-02 22:12:21 +0530

    section 16 main project upto adding http error handling

--- section16MainProjectRevised/src/router.js
+++ section16MainProjectRevised/src/router.js
@@ -5,15 +5,15 @@ import CoachRegistation from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
-import store from './store/index.js';
+//import UserAuth from './pages/auth/UserAuth.vue';
+//import store from './store/index.js';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path:'/',redirect: '/coaches'},
         { path:'/coaches',component: CoachesList },
-        { path:'/coaches/:id',component: CoachDetail,
+        { path:'/coaches/:id',component: CoachDetail, props: true,
             children: [
                 { path:'contact',component: ContactCoach }
             ]
