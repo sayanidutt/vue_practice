cf3489df6074c537b651bd5628e6fee32e7b2e63
Sayani <sayani.dutta@codelogicx.com>
2021-09-06 22:09:56 +0530

    completed authentication part

--- section16MainProjectRevised/src/pages/coaches/CoachesList.vue
+++ section16MainProjectRevised/src/pages/coaches/CoachesList.vue
@@ -10,7 +10,8 @@
       <base-card>
         <div class="controls">
           <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-          <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
+          <base-button link to="/auth?redirect=register" v-if="!isLoggedIn">Login to register as Coach</base-button>
+          <base-button v-if="isLoggedIn && !isCoach && !isLoading" link to="/register">Register as Coach</base-button>
         </div>
         <div v-if="isLoading">
           <base-spinner></base-spinner>
@@ -71,6 +72,9 @@ export default {
     hasCoaches() {
       return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
+    isLoggedIn(){
+      return this.$store.getters.isAuthenticated;
+    }
   },
   created(){
     this.loadCoaches();
--- section16MainProjectRevised/src/router.js
+++ section16MainProjectRevised/src/router.js
@@ -1,12 +1,20 @@
 import { createRouter,createWebHistory } from "vue-router";
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+//import { defineAsyncComponent } from "vue";
+//import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistation from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+//import CoachRegistation from './pages/coaches/CoachRegistration.vue';
+//import ContactCoach from './pages/requests/ContactCoach.vue';
+//import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
-//import store from './store/index.js';
+//import UserAuth from './pages/auth/UserAuth.vue';
+import store from './store/index.js';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistation = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -18,13 +26,24 @@ const router = createRouter({
                 { path:'contact',component: ContactCoach }
             ]
         },
-        { path:'/register',component:CoachRegistation },
-        { path:'/requests',component:RequestsReceived },
-        { path:'/auth',component: UserAuth },
+        { path:'/register',component:CoachRegistation,meta: {requiresAuth: true} },
+        { path:'/requests',component:RequestsReceived,meta: {requiresAuth: true} },
+        { path:'/auth',component: UserAuth,meta: {requiresUnauth: true} },
         { path:'/:notFound(.*)',component:NotFound }
 
 
     ]
 });
+
+router.beforeEach(function(to, _, next) {
+    if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+      next('/auth');
+    } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+      next('/coaches');
+    } else {
+      next();
+    }
+});
+  
 
 export default router;
